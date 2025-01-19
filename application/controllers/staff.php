b87e58e4b9815ec0f980294c3d331cc654c5112a
Abdelrady <abdelrady.mohamed95@hotmail.com>
2025-01-19 17:54:13 +0200

    updates

--- application/controllers/staff.php
+++ application/controllers/staff.php
@@ -21,12 +21,12 @@ class Staff extends CI_Controller
 			redirect('auth/login', 'refresh');
 		}
 
-		// ABDALLAH - Disable Cache
+		// ABDELRADY - Disable Cache
 		// $this->output->set_header('Expires: Sat, 26 Jul 1997 05:00:00 GMT');
 		// $this->output->set_header('Cache-Control: no-store, no-cache, must-revalidate');
 		// $this->output->set_header('Cache-Control: post-check=0, pre-check=0');
 		// $this->output->set_header('Pragma: no-cache');
-		// ABDALLAH - Disable Cache
+		// ABDELRADY - Disable Cache
 	}
 
 	function index()
