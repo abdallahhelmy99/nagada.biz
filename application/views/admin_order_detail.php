7ce93ee1f08af9d553552fe06d2549df425efda4
Abdelrady <abdelrady.mohamed95@hotmail.com>
2025-01-22 17:03:40 +0200

    fix:enter buttons,order validation

--- application/views/admin_order_detail.php
+++ application/views/admin_order_detail.php
@@ -80,7 +80,7 @@
 					$qrname = 'qrcodes/' . $order_id . '.png';
 					file_put_contents($qrname, $qrcode_image);
 				}
-				echo '<img src="/heliopress/qrcodes/' . $qrName . '">';
+				echo '<img src="/qrcodes/' . $qrName . '">';
 
 				?>
 
