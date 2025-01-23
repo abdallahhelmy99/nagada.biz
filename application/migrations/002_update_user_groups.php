a66be70cc21da05883f6cdf1dfb258a2e9a13c17
Abdallah Hussam <76593230+abdallahhelmy99@users.noreply.github.com>
2025-01-23 14:08:51 +0200

    feat:migration: add username column to order_template and modify calendar files table

---
+++ application/migrations/002_update_user_groups.php
@@ -0,0 +1,28 @@
+<?php
+
+/**
+ * Class Migration_update_user_groups
+ * This is the default code for altertions in existing tables.
+ * Both up and down must be filled out accordingly to the data needed
+ * Make sure, that type and constraint are filled out, else it will not work.
+ */
+
+// class Migration_update_user_groups extends CI_Migration
+// {
+//     public function up() {
+
+//         $this->db->query("UPDATE `users_groups` 
+//                           SET `group_id` = 4
+//                           WHERE `group_id` = 3;");
+      
+//     }
+      
+      
+//     public function down() {
+      
+//         $this->db->query("UPDATE `user_groups`  
+//                           SET `group_id` = 3
+//                           WHERE `group_id` = 4;");
+      
+//     }
+// }
